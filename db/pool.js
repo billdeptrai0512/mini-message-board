3404c70cdda65d49a8870cdf0894cb99878ca173
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-19 02:13:25 +0700

    ye

--- db/pool.js
+++ db/pool.js
@@ -7,5 +7,4 @@ module.exports = new Pool({
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    ssl: 'require',
 });
