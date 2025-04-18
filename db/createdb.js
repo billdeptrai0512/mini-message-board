cb263cdf8f91c342ae44cefa341074b7a8611f11
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-19 01:17:59 +0700

    script to create message db

--- db/createdb.js
+++ db/createdb.js
@@ -1,5 +1,9 @@
 const { Client } = require("pg");
 
+const firstSQL = `
+    CREATE DATABASE messages;
+`
+
 const SQL = `
 CREATE TABLE IF NOT EXISTS messages (
   id SERIAL PRIMARY KEY,
@@ -20,9 +24,10 @@ VALUES
 async function main() {
     console.log("seeding...");
     const client = new Client({
-      connectionString: "postgresql://postgres@localhost:5432/messages",
+      connectionString: "postgres://billdeptrai0512:npg_PrGkWt5i4aKS@ep-bitter-surf-a1z5j8j7.ap-southeast-1.pg.koyeb.app/koyebdb",
     });
     await client.connect();
+    await client.query(firstSQL);
     await client.query(SQL);
     await client.end();
     console.log("done");
