d7bab82d3945481178c310867c8cb2e390a756b9
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-19 10:51:07 +0700

    ssl mode

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,6 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-    connectionString: process.env.DB_URL
+    connectionString: process.env.DB_URL,
+    ssl: 'require' 
 });
